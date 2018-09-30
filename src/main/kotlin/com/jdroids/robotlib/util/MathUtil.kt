3145f09f076ae914f8c6780bb750901e430b073b
dansman805 <dansman805@gmail.com>
2018-09-29 23:35:14 -0400

    Updated documentation

--- src/main/kotlin/com/jdroids/robotlib/util/MathUtil.kt
+++
@@ -1,8 +0,0 @@
-package com.jdroids.robotlib.util
-
-/**
- * A singleton that contains a bunch of math lambdas
- */
-object MathUtil {
-    val clamp = {value: Double, low: Double, high: Double -> Math.max(low, Math.min(value, high))}
-}
